08464b997f17167c7c63df2e3917e44102507b38
ndenny <nick@apimetrics.com>
2019-11-03 15:52:53 -0800

    Show build steps

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const { IncomingWebhook } = require('@slack/webhook');
 
 const url = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(url);
-
+const colorMap = {
+  SUCCESS: 'good',
+  FAILURE: 'danger',
+  INTERNAL_ERROR: 'warning',
+  TIMEOUT: 'warning',
+};
 
 // eventToBuild transforms pubsub event message to a build object.
 const eventToBuild = (data) => JSON.parse(Buffer.from(data, 'base64').toString());
@@ -12,19 +17,18 @@ const eventToBuild = (data) => JSON.parse(Buffer.from(data, 'base64').toString()
 const createSlackMessage = (build) => {
   const duration = humanizeDuration(new Date(build.finishTime) - new Date(build.startTime));
   const message = {
-    text: `Build \`${build.id}\` completed in ${duration}`,
+    text: `Build \`${build.id}\`: ${build.status} (after ${duration})`,
     mrkdwn: true,
     attachments: [{
       title: 'Build logs',
       title_link: build.logUrl,
-      fields: [{
-        title: 'Status',
-        value: build.status,
-      },
-      ...Object.entries(build).map((title, value) => ({
-        title,
-        value,
-      })),
+      color: colorMap[build.status],
+      fields: [
+        ...build.steps.map((step) => ({
+          title: `${step.name} ${step.entrypoint} ${step.args.join(' ')}`,
+          value: step.status,
+          color: colorMap[step.status],
+        })),
       ],
     },
     ],
@@ -35,7 +39,7 @@ const createSlackMessage = (build) => {
 // subscribeSlack is the main function called by Cloud Functions.
 module.exports.subscribeSlack = (pubSubEvent) => {
   const build = eventToBuild(pubSubEvent.data);
-
+  console.info(JSON.stringify(build));
   // Skip if the current status is not in the status list.
   // Add additional statuses to list if you'd like:
   // QUEUED, WORKING, SUCCESS, FAILURE,
